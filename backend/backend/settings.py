3cefe1935019de9ed654021a9d7a9dc8ad8ae850
Max Astashev <aerobean@gmail.com>
2023-04-20 18:34:31 +0200

    updated postgres port

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', default='5234'),
+        'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
